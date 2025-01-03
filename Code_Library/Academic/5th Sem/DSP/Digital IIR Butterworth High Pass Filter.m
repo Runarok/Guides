06b4f16120260b985dd127a3e8410550159f1766
Runarok <shreeneethreddy@gmail.com>
2025-01-03 23:06:22 +0530

    Update Digital IIR Butterworth High Pass Filter.m

--- Code_Library/Academic/5th Sem/DSP/Digital IIR Butterworth High Pass Filter.m
+++ Code_Library/Academic/5th Sem/DSP/Digital IIR Butterworth High Pass Filter.m
@@ -1,37 +1,60 @@
 clc;
 clear all;
 close all;
-rp=input('enter the pass band ripple=');
-rs=input('enter the stop band ripple=');
-wp=input('enter the pass band frequency=');
-ws=input('enter the stop band frequency=');
-fs=input('enter the sampling frequency=');
- w1=2*wp/fs;
- w2=2*ws/fs;
- [n,wn]=buttord(w1,w2,rp,rs);
- [b,a]=butter(n,wn,'high');
-disp('the order of hpf');
+
+% Taking input from the user
+rp = input('Enter the passband ripple (in dB): ');  % Passband ripple (dB)
+rs = input('Enter the stopband ripple (in dB): ');  % Stopband ripple (dB)
+wp = input('Enter the passband frequency (in Hz): '); % Passband frequency (Hz)
+ws = input('Enter the stopband frequency (in Hz): '); % Stopband frequency (Hz)
+fs = input('Enter the sampling frequency (in Hz): ');  % Sampling frequency (Hz)
+
+% Normalize the frequencies (0 to pi) for digital filter design
+w1 = 2 * wp / fs;  % Normalized passband frequency
+w2 = 2 * ws / fs;  % Normalized stopband frequency
+
+% Design the Butterworth filter using buttord and butter functions
+[n, wn] = buttord(w1, w2, rp, rs);  % Calculate the order and cutoff frequency
+[b, a] = butter(n, wn, 'high');  % Get the filter coefficients for the high-pass filter
+
+% Display the results
+disp('The order of the high-pass filter:');
 disp(n);
-disp('the cut off freq of hpf');
+disp('The cutoff frequency of the high-pass filter (normalized):');
 disp(wn);
- w=0:0.01:pi;
- [h]=freqz(b,a,w);
-mag=20*log10(abs(h));
-ang=angle(h);
-subplot(2,1,1);
-plot(w/pi,mag);
-xlabel('normalized freq');
-ylabel('magnitude');
-subplot(2,1,2);
-plot(w/pi,ang);
-xlabel('normalisedfreq');
-ylabel('angle');
+
+% Frequency response calculation for plotting
+w = 0:0.01:pi;  % Frequency range from 0 to pi
+[h] = freqz(b, a, w);  % Compute the frequency response of the filter
+
+% Magnitude and Phase of the frequency response
+mag = 20 * log10(abs(h));  % Magnitude in dB
+ang = angle(h);  % Phase angle in radians
+
+% Plotting the magnitude and phase response
+figure;
+
+% Magnitude plot
+subplot(2, 1, 1);
+plot(w / pi, mag);
+xlabel('Normalized Frequency (\pi rad/sample)');
+ylabel('Magnitude (dB)');
+title('Magnitude Response of High-pass Filter');
+grid on;
+
+% Phase plot
+subplot(2, 1, 2);
+plot(w / pi, ang);
+xlabel('Normalized Frequency (\pi rad/sample)');
+ylabel('Phase (radians)');
+title('Phase Response of High-pass Filter');
+grid on;
 
 %%
-system input:
-enter the pass band ripple=3
-enter the stop band ripple=60
-enter the pass band frequency=150
-enter the stop band frequency=300
-enter the sampling frequency=1500           
+Example input:
+Enter the passband ripple (in dB): 3
+Enter the stopband ripple (in dB): 60
+Enter the passband frequency (in Hz): 150
+Enter the stopband frequency (in Hz): 300
+Enter the sampling frequency (in Hz): 1500
 %%
