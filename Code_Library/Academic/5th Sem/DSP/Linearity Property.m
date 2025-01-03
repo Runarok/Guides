d1dc68e54cd1c5e2ccaa0d236fcc9dc61cb572a1
Runarok <shreeneethreddy@gmail.com>
2025-01-03 23:16:31 +0530

    Update Linearity Property.m

--- Code_Library/Academic/5th Sem/DSP/Linearity Property.m
+++ Code_Library/Academic/5th Sem/DSP/Linearity Property.m
@@ -1,24 +1,44 @@
 clc;
 clear all;
 close all;
-x1=input('enter the first input sequence=');
-x2=input('enter the second input sequence=');
-a1=input('enter the constant a1=');
-a2=input('enter the constant a2=');
-l1=length(x1);
-l2=length(x2);
-N=max(l1,l2);
-x1n=[x1,zeros(1,N-l1)];
-x2n=[x2,zeros(1,N-l2)];
-y=a1*x1n+a2*x2n;
-yk=fft(y,N);
-disp('output sequence y(k) is');
+
+% Input sequences from the user
+x1 = input('Enter the first input sequence x1 = ');
+x2 = input('Enter the second input sequence x2 = ');
+a1 = input('Enter the constant a1 = ');
+a2 = input('Enter the constant a2 = ');
+
+% Length of the input sequences
+l1 = length(x1);
+l2 = length(x2);
+
+% Choose the maximum length for zero-padding
+N = max(l1, l2);
+
+% Zero padding the input sequences to length N
+x1n = [x1, zeros(1, N - l1)];
+x2n = [x2, zeros(1, N - l2)];
+
+% Compute the linear combination in time domain
+y = a1 * x1n + a2 * x2n;
+
+% Compute the FFT of the output sequence y in time domain
+yk = fft(y, N);
+
+% Display the FFT of the output sequence y(k)
+disp('Output sequence y(k) is:');
 disp(yk);
-x1k=fft(x1,N);
-x2k=fft(x2,N);
-yv=a1*x1k+a2*x2k;
-if(yk==yv)
-    disp('linearity property is satisfied');
-    else
-      disp('linearity property is not satisfied');  
-end;
+
+% Compute the FFT of the individual input sequences x1 and x2
+x1k = fft(x1n, N);
+x2k = fft(x2n, N);
+
+% Compute the linear combination in frequency domain
+yv = a1 * x1k + a2 * x2k;
+
+% Check if the linearity property holds (if both results are equal)
+if all(abs(yk - yv) < 1e-10)  % Use a small tolerance to account for numerical errors
+    disp('Linearity property is satisfied');
+else
+    disp('Linearity property is NOT satisfied');
+end
